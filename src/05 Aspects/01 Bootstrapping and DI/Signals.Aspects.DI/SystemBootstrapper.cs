863e580683251cf81f031c96bc8bdd0f22c2a136
vojdan@emitknowledge.com <vojdan@emitknowledge.com>
2018-11-28 14:35:32 +0100

    Fix bootstrapper registering exporting types

--- src/05 Aspects/01 Bootstrapping and DI/Signals.Aspects.DI/SystemBootstrapper.cs
+++ src/05 Aspects/01 Bootstrapping and DI/Signals.Aspects.DI/SystemBootstrapper.cs
@@ -18,7 +18,7 @@ namespace Signals.Aspects.DI
         private static IServiceContainer _serviceContainer;
 
         /// <summary>
-        /// Inject all public properties annotated with <see cref="ImportAttribute"/> 
+        /// Inject all public properties annotated with <see cref="ImportAttribute"/>
         /// </summary>
         /// <param name="obj"></param>
         public static void Bootstrap(object obj)
@@ -48,12 +48,12 @@ namespace Signals.Aspects.DI
             return _serviceContainer.GetInstance(serviceType);
         }
 
-		/// <summary>
-		/// Initialize with container registrator that results in service container
-		/// </summary>
-		/// <param name="registrationService"></param>
-		/// <param name="scanAssemblies"></param>
-		public static IServiceContainer Init(IRegistrationService registrationService, params Assembly[] scanAssemblies)
+        /// <summary>
+        /// Initialize with container registrator that results in service container
+        /// </summary>
+        /// <param name="registrationService"></param>
+        /// <param name="scanAssemblies"></param>
+        public static IServiceContainer Init(IRegistrationService registrationService, params Assembly[] scanAssemblies)
         {
             if (scanAssemblies != null && scanAssemblies.Length > 0)
                 AutoRegister(registrationService, scanAssemblies);
@@ -62,32 +62,32 @@ namespace Signals.Aspects.DI
             return _serviceContainer;
         }
 
-		/// <summary>
-		/// Auto register all exported types
-		/// </summary>
-		/// <param name="registrationService"></param>
-		/// <param name="scanAssemblies"></param>
-		private static void AutoRegister(IRegistrationService registrationService, params Assembly[] scanAssemblies)
+        /// <summary>
+        /// Auto register all exported types
+        /// </summary>
+        /// <param name="registrationService"></param>
+        /// <param name="scanAssemblies"></param>
+        private static void AutoRegister(IRegistrationService registrationService, params Assembly[] scanAssemblies)
         {
             // get all types from all assemblies
-            var types = scanAssemblies.SelectMany(x => x.GetExportedTypes().Concat(
+            var types = scanAssemblies.SelectMany(x => x.GetTypes().Concat(
                                 x.GetReferencedAssemblies()
                                .Select(Assembly.Load)
-                               .SelectMany(assembly => assembly.GetExportedTypes()))).ToList();
+                               .SelectMany(assembly => assembly.GetTypes()))).ToList();
 
             // get all types with exported attribute
-            var exportedTypePairs = types
+            var typePairs = types
                 .Select(x => new KeyValuePair<Type, ExportAttribute>(x, x.GetCustomAttribute<ExportAttribute>()))
                 .Where(x => x.Value != null)
                 .ToList();
 
             // get distinct exports
-            var distinctExports = exportedTypePairs.Select(x => x.Value?.DefinitionType.FullName).Distinct().ToList();
+            var distinctExports = typePairs.Select(x => x.Value?.DefinitionType.FullName).Distinct().ToList();
 
-            if (exportedTypePairs.Count != distinctExports.Count)
+            if (typePairs.Count != distinctExports.Count)
             {
                 // all groups that have more than 1 type with same implemented interface type
-                var groups = exportedTypePairs.GroupBy(x => x.Value).Where(x => x.Count() > 1);
+                var groups = typePairs.GroupBy(x => x.Value).Where(x => x.Count() > 1);
                 foreach (var group in groups)
                 {
                     throw new Exception($"{group.Key.DefinitionType.FullName} has multiple implementations: ");
@@ -96,7 +96,7 @@ namespace Signals.Aspects.DI
             else
             {
                 // autoregister all types
-                foreach (var pair in exportedTypePairs)
+                foreach (var pair in typePairs)
                 {
                     registrationService.Register(pair.Value.DefinitionType, pair.Key);
                 }
