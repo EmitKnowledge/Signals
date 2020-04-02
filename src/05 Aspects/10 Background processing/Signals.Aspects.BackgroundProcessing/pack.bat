594b441c2c3e16a4d1abf515ba81273ddc7d2343
Marjan Nikolovski <marjan@emitknowledge.com>
2020-04-02 11:26:55 +0200

    - added exit

--- src/05 Aspects/10 Background processing/Signals.Aspects.BackgroundProcessing/pack.bat
+++ src/05 Aspects/10 Background processing/Signals.Aspects.BackgroundProcessing/pack.bat
@@ -9,3 +9,4 @@ SET ApiKey=VSTS
 for %%i in (bin\Release\*.nupkg) do del "%%i"
 dotnet msbuild /t:pack /p:Configuration=Release /p:Version=%Version% /p:PackageVersion=%Version%
 for %%i in (bin\Release\*.nupkg) do nuget push -Source %NugetUrl% -ApiKey %ApiKey% "%%i"
+exit
