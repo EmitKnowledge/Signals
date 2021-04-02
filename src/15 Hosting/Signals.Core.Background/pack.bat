fe53601e21a7f6de535ed5cca96315cded303ce9
Aleksandar Taseski <aleksandar.taseski@emitknowledge.com>
2021-04-02 11:35:17 +0200

    Nuget version update

--- src/15 Hosting/Signals.Core.Background/pack.bat
+++ src/15 Hosting/Signals.Core.Background/pack.bat
@@ -1,6 +1,6 @@
 ﻿
 REM !important
-SET Version=1.0.21
+SET Version=1.0.22
 
 
 SET NugetUrl=https://api.nuget.org/v3/index.json
--- src/15 Hosting/Signals.Core.Web/pack.bat
+++ src/15 Hosting/Signals.Core.Web/pack.bat
@@ -1,6 +1,6 @@
 ﻿
 REM !important
-SET Version=1.0.33
+SET Version=1.0.34
 
 
 SET NugetUrl=https://api.nuget.org/v3/index.json
