c4944a382882a1e68d41f3976b1c1c9f5b43327e
Marjan Nikolovski <marjan@emitknowledge.com>
2021-09-17 09:18:35 +0200

    - removed .NET Framework dependencies

--- src/15 Hosting/Signals.Core.Web/Http/SessionProvider.cs
+++ src/15 Hosting/Signals.Core.Web/Http/SessionProvider.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
 using Signals.Core.Common.Instance;
 using Signals.Core.Common.Serialization;
 using Signals.Core.Processing.Input.Http;
@@ -11,59 +10,7 @@ namespace Signals.Core.Web.Http
     /// </summary>
     public class SessionProvider : ISessionProvider
     {
-
-#if (NET461)
-
-        /// <summary>
-        /// Real http context
-        /// </summary>
-        private System.Web.HttpContext _context;
-
-        /// <summary>
-        /// CTOR
-        /// </summary>
-        /// <param name="context"></param>
-        public SessionProvider(System.Web.HttpContext context)
-        {
-            _context = context;
-        }
-
-        /// <summary>
-        /// Sets session value
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="value"></param>
-        public void Set(string name, object value)
-        {
-            if (!_context.Session.IsNull())
-                _context.Session[name] = value.SerializeJson();
-        }
-
-        /// <summary>
-        /// Removes session value
-        /// </summary>
-        /// <param name="name"></param>
-        public void Remove(string name)
-        {
-            _context.Session?.Remove(name);
-        }
-
-        /// <summary>
-        /// Gets session value
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public T Get<T>(string name) where T : class
-        {
-            var sessionValue = _context.Session?[name];
-            if (sessionValue.IsNull()) return (T)null;
-            return sessionValue.ToString().Deserialize<T>(SerializationFormat.Json);
-        }
-
-#else
-
-        /// <summary>
+	    /// <summary>
         /// Real http context
         /// </summary>
         private Microsoft.AspNetCore.Http.HttpContext _context;
@@ -109,8 +56,5 @@ namespace Signals.Core.Web.Http
             if (sessionValue.IsNull()) return (T)null;
             return sessionValue.Deserialize<T>(SerializationFormat.Json);
         }
-
-#endif
-
     }
 }
